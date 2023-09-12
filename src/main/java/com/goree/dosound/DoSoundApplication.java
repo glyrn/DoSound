e2ca884f8a7aca0723c859ad92a008c427522b7a
gly <reen.gly@qq.com>
2023-09-12 18:24:39 +0800

    init

---
+++ src/main/java/com/goree/dosound/DoSoundApplication.java
@@ -0,0 +1,13 @@
+package com.goree.dosound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoSoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoSoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goree/dosound/DoSoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goree.dosound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoSoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
